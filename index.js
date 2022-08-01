61475fa0500fb72c10015112f438e6851ff92867
Pavel Gerasimovic <p.hitman3335@gmail.com>
2022-08-01 20:38:50 +0300

    first push

---
+++ index.js
@@ -0,0 +1,44 @@
+let arr = [
+  {
+    id: 123,
+    task: "buy milk",
+  },
+  {
+    id: 321,
+    task: "buy eggs",
+  },
+  {
+    id: 234,
+    task: "clean my room",
+  },
+  {
+    id: 234,
+    task: "watch any js video on youtube",
+  },
+];
+
+const tasksList = document.getElementById("tasksList");
+
+arr.forEach((value, ind) => {
+  // creating elements
+  const myLi = document.createElement("li");
+  const myInput = document.createElement("input");
+  const myLabel = document.createElement("label");
+
+  // adding styles
+  myLi.className = "list-group-item";
+  myInput.className = "form-check-input me-1";
+  myLabel.className = "form-check-label";
+
+  // adding other attributes
+  myInput.setAttribute("type", "checkbox");
+  myInput.setAttribute("id", value.id);
+
+  // Label
+  myLabel.setAttribute("for", value.id);
+  myLabel.textContent = value.task;
+
+  // append childs
+  myLi.append(myInput, myLabel);
+  tasksList.append(myLi);
+});
