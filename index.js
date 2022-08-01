df7fb2c2d96aec3d33dba375f905ddb24194cc55
Pavel Gerasimovic <p.hitman3335@gmail.com>
2022-08-01 22:03:24 +0300

    done

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
-let arr = [
-  {
-    id: 123,
-    task: "buy milk",
-  },
-  {
-    id: 321,
-    task: "buy eggs",
-  },
-  {
-    id: 234,
-    task: "clean my room",
-  },
-  {
-    id: 234,
-    task: "watch any js video on youtube",
-  },
-];
+let arr = [];
 
-const tasksList = document.getElementById("tasksList");
+var inpat = document.getElementById("value.id");
 
-arr.forEach((value, ind) => {
-  // creating elements
-  const myLi = document.createElement("li");
-  const myInput = document.createElement("input");
-  const myLabel = document.createElement("label");
+document.getElementById("addBtn").addEventListener("click", () => {
+  let inputValue = document.getElementById("taskInput").value;
+  const newTask = {
+    id: new Date().getTime(),
+    task: inputValue,
+  };
 
-  // adding styles
-  myLi.className = "list-group-item";
-  myInput.className = "form-check-input me-1";
-  myLabel.className = "form-check-label";
+  if (inputValue == "") {
+    alert("task field is empty");
+    return false;
+  }
 
-  // adding other attributes
-  myInput.setAttribute("type", "checkbox");
-  myInput.setAttribute("id", value.id);
-
-  // Label
-  myLabel.setAttribute("for", value.id);
-  myLabel.textContent = value.task;
-
-  // append childs
-  myLi.append(myInput, myLabel);
-  tasksList.append(myLi);
+  arr.push(newTask);
+  drawTaskList();
 });
+
+const drawTaskList = () => {
+  const tasksList = document.getElementById("tasksList");
+  tasksList.innerHTML = null;
+  arr.forEach((value, ind) => {
+    // creating elements
+    const myLi = document.createElement("li");
+    const myInput = document.createElement("input");
+    const myLabel = document.createElement("label");
+
+    // adding styles
+    myLi.className = "list-group-item";
+    myInput.className = "form-check-input me-1";
+    myLabel.className = "form-check-label";
+
+    // adding other attributes
+    myInput.setAttribute("type", "checkbox");
+    myInput.setAttribute("id", value.id);
+
+    // Label
+    myLabel.setAttribute("for", value.id);
+    myLabel.textContent = value.task;
+
+    // append childs
+    myLi.append(myInput, myLabel);
+    tasksList.append(myLi);
+  });
+};
